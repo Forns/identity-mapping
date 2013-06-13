256f9d666b9d28880f1695b5b166c0e8e032b8c7
Forns <forneyandrew@gmail.com>
2013-06-13 00:57:53 -0700

    - Initial topic model system in place

--- server.js
+++ server.js
@@ -54,7 +54,11 @@ app.configure('production', function () {
 
 surveyDao = require("./src/main/conf/db-config.js")().surveyDao;
 lda = require("./src/main/conf/lda-config.js")().lda;
-$TM = require("./src/main/conf/topic-modeler-config.js")(lda).$TM;
+$TM = require("./src/main/conf/topic-modeler-config.js")(surveyDao, lda).$TM;
+
+console.log(
+  $TM.getTopics(["Its subfields can be divided into a variety of theoretical and practical disciplines. Some fields, such as computational complexity theory (which explores the fundamental properties of computational problems), are highly abstract, whilst fields such as computer graphics emphasize real-world visual applications. Still other fields focus on the challenges in implementing computation. For example, programming language theory considers various approaches to the description of computation, whilst the study of computer programming itself investigates various aspects of the use of programming language and complex systems. Human-computer interaction considers the challenges in making computers and computations useful, usable, and universally accessible to humans."])
+);
 
 /*
  * CONTROLLERS
--- src/main/conf/topic-modeler-config.js
+++ src/main/conf/topic-modeler-config.js
@@ -1,28 +1,29 @@
 /**
- *  topic-modeler.js
+ * topic-modeler.js
  * 
- *  Provides LDA topic modeling for the Node.js backend 
+ * Provides LDA topic modeling for the Node.js backend
+ * 
+ * Credit to Awais Athar for the conversion to js
+ * http://chaoticity.com/lda-based-topic-modelling-in-javascript/
  */
 
-module.exports = function (db) {
+module.exports = function (db, lda) {
+  var $TM = {};
   
-  /*
-  var topicise = function () {
+  $TM.getTopics = function (sentences) {
     var documents = new Array(),
         f = {},
-        vocab=new Array();
+        vocab = new Array();
     for (var i = 0; i < sentences.length; i++) {
       if (sentences[i] == "") {
         continue;
       }
       documents[i] = new Array();
-      //console.log(sentences[i]);
       var words = sentences[i].split(/[\s,\"]+/);
-      //console.log(words);
       if(!words) continue;
       for(var wc=0;wc<words.length;wc++) {
         var w=words[wc].toLowerCase().replace(/[^a-z\'A-Z0-9 ]+/g, '');
-        if (w=="" || w.length==1 || stopwords[w] || w.indexOf("http")==0) continue;
+        if (w=="" || w.length==1 || w.indexOf("http")==0) continue;
         if (f[w]) { 
           f[w]=f[w]+1;      
         } 
@@ -36,7 +37,7 @@ module.exports = function (db) {
       
     var V = vocab.length;
     var M = documents.length;
-    var K = parseInt($( "#topics" ).val());
+    var K = 3; // Assume 3 topics, for now
     var alpha = 0.1;  // per-document distributions over topics
     var beta = .01;  // per-topic distributions over words
   
@@ -46,101 +47,32 @@ module.exports = function (db) {
     var theta = lda.getTheta();
     var phi = lda.getPhi();
   
-    var text = '';
-  
-    //topics
+    // Topic calculation
     var topTerms=20;
-    var topicText = new Array();
+    var topicText = [];
+    var topicProbs = [];
     for (var k = 0; k < phi.length; k++) {
-      text+='<canvas id="topic'+k+'" class="topicbox color'+k+'"><ul>';
+      topicText[k] = [];
+      topicProbs[k] = [];
       var things = new Array();
       for (var w = 0; w < phi[k].length; w++) {
          things.push(""+phi[k][w].toPrecision(2)+"_"+vocab[w]);
       }
       things.sort().reverse();
-      //console.log(things);
       if(topTerms>vocab.length) topTerms=vocab.length;
-      topicText[k]='';
       for (var t = 0; t < topTerms; t++) {
         var topicTerm=things[t].split("_")[1];
         var prob=parseInt(things[t].split("_")[0]*100);
         if (prob<2) continue;
-        text+=( '<li><a href="javascript:void(0);" data-weight="'+(prob*2)+'" title="'+prob+'%">'+topicTerm +'</a></li>' );     
-        console.log(topicTerm+" = " + prob  + "%");
-        topicText[k] += ( topicTerm +" ");
+        topicProbs[k][t] = prob;
+        topicText[k][t] = topicTerm;
       }
-      text+='</ul></canvas>';
     }
-    $('#topiccloud').html(text);
     
-    text='<div class="spacer"> </div>';
-    // sentences
-    for (var m = 0; m < theta.length; m++) {
-      text+='<div class="tweet">';
-      text+='<img class="dp" src="'+tweets[m].profile_image_url+'"/>';
-      text+=tweets[m].text+'<br/>';
-      for (var k = 0; k < theta[m].length; k++) {
-        text+=('<div class="box bgcolor'+k+'" style="width:'+parseInt(""+(theta[m][k]*100))+'px" title="'+topicText[k]+'"></div>');
-      }
-      text+='</div>';
-    }
-  
-    $("#output").html(twttr.txt.autoLink(text));
-  
-    for (var k = 0; k < phi.length; k++) {
-      if(!$('#topic'+k).tagcanvas({
-            textColour: $('#topic'+k).css('color'),
-          maxSpeed: 0.03,
-          initial: [0.1,-0.1],
-          decel: 0.98,
-          reverse: true,
-          weightSize:2,
-          weightMode:'size',
-          weightFrom:'data-weight',
-          weight: true
-        })) 
-      {
-        $('#topic'+k).hide();
-      } 
-    }
+    return {topics: topicText, probabilities: topicProbs}
   }
   
-  $(document).ready(function(){
-    var select = $( "#topics" );
-    var slider = $( "<div id='slider'></div>" ).insertAfter( select ).slider({
-      min: 2,
-      max: 10,
-      range: "min",
-      value: select[0].selectedIndex+2,
-      slide: function( event, ui ) {
-        select[0].selectedIndex = ui.value-2;
-      }
-    });
-    $( "#topics" ).change(function() {
-      slider.slider( "value", this.selectedIndex + 2 );
-    });
-  });
-  
-  function doit() {
-    $('#btnTopicise').attr('disabled','disabled');
-    $.ajax( 
-      {url:'http://search.twitter.com/search.json?rpp=100&include_entities=false&include_rts=false&q='+encodeURIComponent($('#username').val()),
-      dataType:'jsonp',
-      success:function(data) {
-        sentences = new Array();
-        tweets=new Array();
-        $(data.results).each(function(i,status) {
-          sentences.push($("<div/>").html(status.text).text());
-          tweets.push(status);
-        });
-        topicise();
-        $('#btnTopicise').removeAttr('disabled');
-        
-      }
-    });
-    
-  }
-  */
+  this.$TM = $TM;
   
   return this;
 };
