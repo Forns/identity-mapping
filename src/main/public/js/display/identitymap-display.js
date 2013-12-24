a5eb55cf6175956fc3aac1e26c76ea7a705aebaa
Dondi Dionisio <dondi@lmu.edu>
2013-12-24 13:46:09 -0800

    Major restructuring phase 1: Add archdomains as a distinct array layer, and adjust d3 code accordingly.

--- src/main/public/js/display/identitymap-display.js
+++ src/main/public/js/display/identitymap-display.js
@@ -44,17 +44,18 @@ $(function () {
         // http://bl.ocks.org/mbostock/5415941
         surveyToSystems = function (survey) {
             // Special mappings.
-            var ONE_LEVEL_SYSTEMS = [ "Blogs", "Emails" ],
-
-                TWO_LEVEL_SYSTEMS = [
+            var ARCHDOMAINS = [
+                    "Blogs",
+                    "Emails",
                     "Social Networks",
                     "Online Dating Sites",
                     "Online Forums",
                     "Digital Gaming Platforms",
                     "3D Virtual Worlds"
-                    // TODO Fill this out based on survey modules; heck, maybe even centralize this
-                    //      so that *both* the visualization and the survey are reading the same
-                    //      data.
+                    // TODO Find a way to centralize this so that *both* the visualization and the
+                    //      survey are reading the same data (i.e., ideally we shouldn't have to
+                    //      change multiple source files if we decide to add a new archdomain in
+                    //      the future.
                 ],
 
                 PERIODICITIES = {
@@ -70,76 +71,51 @@ $(function () {
                 // The final result.
                 systems = [];
 
-            // Each archdomain in the survey translates into a "system."
-            // Each domain translates into a satellite around that system.
-            // Each account within a domain translates into a subsatellite of that satellite.
+            // Each archdomain in the survey translates into a "system domain."
+            // Each domain translates into a "planet" within that system domain.
+            // Each account within a domain translates into a "moon" of that "planet."
             // The frequency of use of a particular domain/account translates to periodicity.
-            // The number of domains naturally "enlarges" a system because it increases
-            // the number of satellites therein.
-            ONE_LEVEL_SYSTEMS.forEach(function (systemName) {
-                var systemSource = survey[systemName];
-
-                // If that archdomain is present, then we go down another level.
-                systemSource = systemSource ? systemSource[systemName] : {
-                    purpose: "(n/a)"
-                };
-                systems.push({
-                    key: systems.length,
-                    system_name: systemName,
-                    planet_name: systemName,
-                    purpose: systemSource.purpose,
-                    values: Object.keys(systemSource).filter(function (key) {
-                        return key.match(FREQUENCY_REGEX);
-                    }).map(function (frequencyKey, index) {
-                        return {
-                            id: systems.length,
-                            period: PERIODICITIES[systemSource[frequencyKey]],
-                            planet_name: systemName + frequencyKey.match(FREQUENCY_INDEX_REGEX),
-                            planet_radius: MOON_RADIUS,
-                            semimajor_axis: (index + ORBIT_START) * ORBIT_STEP,
-                        };
-                    })
-                });
-            });
-
-            // Two-level systems show multiple "planets" within the same orbit.
+            //
+            // We multiple "planets" within the same archdomain on the same orbit.
             // This serves as the chosen alternative to the other options, which
             // are to either put multiple orbits in the same region (original version)
             // or to show two-level systems as satellites with satellites.
-            TWO_LEVEL_SYSTEMS.forEach(function (systemName) {
-                var systemSource = survey[systemName];
-                if (!systemSource) {
-                    systems.push({
+            ARCHDOMAINS.forEach(function (domainName) {
+                var systemDomain = {
                         key: systems.length,
-                        system_name: systemName,
-                        planet_name: systemName,
-                        purpose: "(n/a)",
-                        values: []
-                    });
+                        domainName: domainName,
+                        planets: []
+                    },
+
+                    systemSource = survey[domainName];
+
+                systems.push(systemDomain);
+
+                // If no domains, then we're done.
+                if (!systemSource) {
                     return;
                 }
 
-                Object.keys(systemSource).forEach(function (subsystemName) {
-                    // TODO This is a near-identical copy of the code above.  Yes, we need to fix this.
+                // For every domain, add a planet.
+                systemDomain.planets = Object.keys(systemSource).map(function (subsystemName, index) {
                     var subsystemSource = systemSource[subsystemName];
-
-                    systems.push({
-                        key: systems.length,
-                        system_name: systemName,
-                        planet_name: subsystemSource.definition || subsystemName,
+                    return {
+                        key: index,
+                        planetName: subsystemSource.definition || subsystemName,
+                        planetRadius: PLANET_RADIUS,
                         purpose: subsystemSource.purpose,
-                        values: Object.keys(subsystemSource).filter(function (key) {
+                        moons: Object.keys(subsystemSource).filter(function (key) {
                             return key.match(FREQUENCY_REGEX);
-                        }).map(function (frequencyKey, index) {
+                        }).map(function (frequencyKey, moonIndex) {
                             return {
-                                id: systems.length,
+                                id: moonIndex,
                                 period: PERIODICITIES[subsystemSource[frequencyKey]],
-                                planet_name: subsystemName + frequencyKey.match(FREQUENCY_INDEX_REGEX),
-                                planet_radius: MOON_RADIUS,
-                                semimajor_axis: (index + ORBIT_START) * ORBIT_STEP,
+                                moonName: subsystemName + frequencyKey.match(FREQUENCY_INDEX_REGEX),
+                                moonRadius: MOON_RADIUS,
+                                semimajorAxis: (index + ORBIT_START) * ORBIT_STEP,
                             };
                         })
-                    });
+                    };
                 });
             });
 
@@ -153,23 +129,34 @@ $(function () {
         totalRadius = Math.max($sun.width(), $sun.height()); // Margin for sun.
 
         // Compute the "distances" of each system from the "sun."
-        systems.forEach(function (s) {
-            s.values.forEach(function (p) { p.system = s; });
-
+        systems.forEach(function (systemDomain) {
             // Radius is the larger of the base radius (which an empty region would get)
             // or the space needed by the planet with the most satellites.
             var regionRadius = x(ORBIT_START * ORBIT_STEP) + r(MOON_RADIUS),
-                planetRadius = d3.max(s.values, function (p) { return x(p.semimajor_axis) + r(p.planet_radius); }) || 0;
+                planetRadius = d3.max(systemDomain.planets, function (planet) {
+                    planet.radius = d3.max(planet.moons, function (moon) {
+                        return x(moon.semimajorAxis) + r(moon.moonRadius)
+                    });
+                    return planet.radius;
+                }) || 0;
 
-            s.radius = Math.max(regionRadius, planetRadius) + padding;
-            s.distance = totalRadius + s.radius;
-            totalRadius += s.radius * 2;
+            systemDomain.radius = Math.max(regionRadius, planetRadius) + padding;
+            systemDomain.distance = totalRadius + systemDomain.radius;
+            totalRadius += systemDomain.radius * 2;
+
+            systemDomain.planets.forEach(function (planet) {
+                planet.distance = systemDomain.distance;
+                planet.system = systemDomain;
+                planet.moons.forEach(function (moon) {
+                    moon.system = planet;
+                });
+            });
         });
 
         // We want to process our systems from the furthest to the closest to that the smallest orbits
         // are "on top."
-        systems.sort(function (system1, system2) {
-            return system2.distance - system1.distance;
+        systems.sort(function (systemDomain1, systemDomain2) {
+            return systemDomain2.distance - systemDomain1.distance;
         });
 
         // Resize our solar system according to the total calculated radius.
@@ -185,8 +172,7 @@ $(function () {
         });
 
         // TODO Lots and lots and lots of consolidation can be done here.
-        var systemDomainLabelCheck = {},
-            systemDomain = d3.select("#main-content").selectAll(".system-domain")
+        var systemDomain = d3.select("#main-content").selectAll(".system-domain")
             .data(systems)
             .enter().append("div")
             .attr('class', "system-domain")
@@ -196,18 +182,12 @@ $(function () {
             .style('top', function (d) { return totalRadius - d.distance - d.radius + "px"; });
 
         systemDomain.append("div")
-            .attr('class', function (d) { return "label " + domainToId(d.system_name); })
-            .text(function (d) {
-                // We want to apply a label just once.
-                var label = systemDomainLabelCheck[d.system_name] ? "" : d.system_name;
-                systemDomainLabelCheck[d.system_name] = true;
-                return label;
-            })
+            .attr('class', function (d) { return "label " + domainToId(d.domainName); })
+            .text(function (d) { return d.domainName; })
             .style('top', function (d) { return d.radius + 24 + "px"; });
 
         systemDomain = systemDomain.append("div")
             .attr('class', "region")
-            .attr('title', function (d) { return d.purpose || "(purpose not stated)"; })
             .style('width', function (d) { return (d.distance + d.radius) * 2 + "px"; })
             .style('height', function (d) { return (d.distance + d.radius) * 2 + "px"; })
             .style(prefix + "border-radius", function (d) { return d.distance + d.radius + "px"; })
@@ -215,7 +195,7 @@ $(function () {
             .style(prefix + "transform-origin", function (d) { return (d.distance + d.radius) + "px " + (d.distance + d.radius) + "px"; });
 
         systemDomain.append("svg")
-            .attr('class', function (d) { return "system-orbit " + domainToId(d.system_name); })
+            .attr('class', function (d) { return "system-orbit " + domainToId(d.domainName); })
             .attr('width', function (d) { return d.distance * 2; })
             .attr('height', function (d) { return d.distance * 2; })
             .style('left', function (d) { return d.radius + "px"; })
@@ -226,10 +206,11 @@ $(function () {
             .attr('cy', function (d) { return d.distance; })
             .attr('r', function (d) { return d.distance; });
 
-        var system = systemDomain.append("div")
+        var system = systemDomain.selectAll(".system")
+            .data(function (d) { return d.planets; })
+            .enter().append("div")
             .attr('class', "system")
-            // This is invisible if there are no listed accounts.
-            .style('display', function (d) { return d.values.length ? "block" : "none"; })
+            .attr('title', function (d) { return d.purpose || "(purpose not stated)"; })
             .style('width', function (d) { return d.radius * 2 + "px"; })
             .style('height', function (d) { return d.radius * 2 + "px"; })
             .style('left', function (d) { return d.distance + d.distance + "px"; })
@@ -245,8 +226,8 @@ $(function () {
             .attr('height', function (d) { return d.radius * 2; })
             .append("circle")
             .attr('transform', function (d) { return "translate(" + d.radius + "," + d.radius + ") scale(1.0,2.0)"; })
-            .attr('r', function (d) { return r(PLANET_RADIUS); })
-            .style('fill', function (d) { return "url(#" + domainToId(d.system_name) + "-gradient)"; });
+            .attr('r', function (d) { return r(d.planetRadius); })
+            .style('fill', function (d) { return "url(#" + domainToId(d.system.domainName) + "-gradient)"; });
 
         system.append("div")
             .attr('class', "label")
@@ -254,25 +235,25 @@ $(function () {
             .style('top', function (d) { return d.radius + 10 + "px"; })
             .style(prefix + "animation-duration", function (d) { return t(d.distance / 4) + "s"; })
             .append("span")
-            .text(function (d) { return d.planet_name; });
+            .text(function (d) { return d.planetName; });
 
         system.append("svg")
             .attr('class', "orbit")
             .attr('width', function (d) { return d.radius * 2; })
             .attr('height', function (d) { return d.radius * 2; })
             .append("g")
-            .attr("transform", function (d) { return "translate(" + d.radius + "," + d.radius + ")"; })
+            .attr('transform', function (d) { return "translate(" + d.radius + "," + d.radius + ")"; })
             .selectAll("circle")
-            .data(function (d) { return d.values; })
+            .data(function (d) { return d.moons; })
             .enter().append("circle")
-            .attr("r", function (d) { return x(d.semimajor_axis); });
+            .attr('r', function (d) { return x(d.semimajorAxis); });
 
         system.selectAll(".moon")
-            .data(function (d) { return d.values; })
+            .data(function (d) { return d.moons; })
             .enter().append("svg")
-            .attr("class", "moon")
-            .attr("width", function (d) { return d.system.radius * 2; })
-            .attr("height", function (d) { return d.system.radius * 2; })
+            .attr('class', "moon")
+            .attr('width', function (d) { return d.system.radius * 2; })
+            .attr('height', function (d) { return d.system.radius * 2; })
             .style(prefix + "animation-duration", function (d) {
                 // "Perturb" periodicity by up to 2 days; this adds some variation while
                 // retaining differences in frequencies.
@@ -282,7 +263,7 @@ $(function () {
             .style(prefix + "transform-origin", function (d) { return d.system.radius + "px " + d.system.radius + "px"; })
             .append("g")
             .attr('class', "orbiter")
-            .attr("transform", function (d) { return "translate(" + (d.system.radius + x(d.semimajor_axis)) + "," + d.system.radius + ")"; })
+            .attr('transform', function (d) { return "translate(" + (d.system.radius + x(d.semimajorAxis)) + "," + d.system.radius + ")"; })
             .append("g")
             .attr('class', "perspective-adjust")
             .style(prefix + "animation-duration", function (d) {
@@ -295,6 +276,6 @@ $(function () {
             })
             .append("circle")
             .style(prefix + "transform", function (d) { return "scale(1.0,2.0)"; })
-            .attr("r", function (d) { return r(d.planet_radius); });
+            .attr('r', function (d) { return r(d.moonRadius); });
     });
 });
