00f6eabe38cf98b30e7ebdb1dda1ec04603db948
Dondi Dionisio <dondi@lmu.edu>
2014-06-06 15:45:17 -0700

    Attempt at doing iDevice-sensitive code.

--- src/main/public/js/display/identitymap-display.js
+++ src/main/public/js/display/identitymap-display.js
@@ -421,6 +421,11 @@ $(function () {
             .attr('stroke', getGradientUrl)
             .attr('marker-end', function (d, i) { return "url(#crossover-marker-" + i + ")"; });
 
+        // iPhone/iPad/iPod compatibility check---kill animation (and skip animating crossovers).
+        if (/(iPad|iPhone|iPod)/g.test(navigator.userAgent)) {
+            $(".nebula-holder").remove();
+        }
+
 // http://paulirish.com/2011/requestanimationframe-for-smart-animating/
 // http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
  
