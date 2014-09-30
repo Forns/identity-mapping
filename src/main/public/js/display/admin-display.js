9cb01297643611ca9b3761206d7cdb7f733f36a1
Forns <forneyandrew@gmail.com>
2014-09-29 20:38:40 -0700

    - Small fix to avg age display on admin panel

--- src/main/public/js/display/admin-display.js
+++ src/main/public/js/display/admin-display.js
@@ -8,7 +8,7 @@ $(function () {
     type: "GET",
     success: function (results, textStatus, jqXHR) {
       $("#count").text(results.count);
-      $("#avgAge").text(results.avgAge);
+      $("#avgAge").text(results.avgAge.toFixed(2));
       $("#avgMale").text(results.avgMale.toFixed(2));
       $("#avgFemale").text((1 - results.avgMale).toFixed(2));
     }
